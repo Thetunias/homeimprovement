d926cf8d19867e8f1fe8c4632f07ae745e3bf93a
Cain Watson <cainwatson00@gmail.com>
2020-07-17 17:19:43 -0500

    style: fix formatting in test

--- api/test/home_improvement_web/views/error_view_test.exs
+++ api/test/home_improvement_web/views/error_view_test.exs
@@ -5,7 +5,9 @@ defmodule HomeImprovementWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(HomeImprovementWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(HomeImprovementWeb.ErrorView, "404.json", []) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500.json" do
