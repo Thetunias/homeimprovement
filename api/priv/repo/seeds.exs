ba18e3350ba5dfd0479c22f13dfcfc649cde743a
Cain Watson <cainwatson00@gmail.com>
2020-07-29 19:39:33 -0500

    chore: add seed users

--- api/priv/repo/seeds.exs
+++ api/priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+import HomeImprovement.AccountsFactory
+
+create_user()
+create_user()
