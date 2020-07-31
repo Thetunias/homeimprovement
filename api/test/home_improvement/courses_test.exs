f64d5f46e3ffc1cf3c1566201d4f746afbf86a67
Cain Watson <cainwatson00@gmail.com>
2020-07-30 19:32:32 -0500

    test: update courses tests to use factory

--- api/test/home_improvement/courses_test.exs
+++ api/test/home_improvement/courses_test.exs
@@ -1,63 +1,77 @@
 defmodule HomeImprovement.CoursesTest do
   use HomeImprovement.DataCase
 
+  import HomeImprovement.AccountsFactory,
+    only: [
+      create_user: 1
+    ]
+
+  import HomeImprovement.CoursesFactory,
+    only: [
+      create_course: 1
+    ]
+
   alias HomeImprovement.Courses
+  alias HomeImprovement.Courses.Course
 
-  describe "courses" do
-    alias HomeImprovement.Courses.Course
+  describe ".list_courses/0" do
+    setup [:create_user, :create_course]
 
-    @valid_attrs %{name: "some name"}
-    @update_attrs %{name: "some updated name"}
-    @invalid_attrs %{name: nil}
-
-    def course_fixture(attrs \\ %{}) do
-      {:ok, course} =
-        attrs
-        |> Enum.into(@valid_attrs)
-        |> Courses.create_course()
-
-      course
-    end
-
-    test "list_courses/0 returns all courses" do
-      course = course_fixture()
+    test "returns all courses", %{course: course} do
       assert Courses.list_courses() == [course]
     end
+  end
 
-    test "get_course!/1 returns the course with given id" do
-      course = course_fixture()
+  describe ".get_course!/1" do
+    setup [:create_user, :create_course]
+
+    test "returns the course with given id", %{course: course} do
       assert Courses.get_course!(course.id) == course
     end
+  end
 
-    test "create_course/1 with valid data creates a course" do
-      assert {:ok, %Course{} = course} = Courses.create_course(@valid_attrs)
-      assert course.name == "some name"
+  describe ".create_course/1" do
+    setup [:create_user]
+
+    test "creates a course", %{user: user} do
+      assert {:ok, %Course{} = course} =
+               Courses.create_course(%{name: "Gardening", id_user_creator: user.id})
+
+      assert course.name == "Gardening"
     end
 
-    test "create_course/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Courses.create_course(@invalid_attrs)
+    test "returns error changeset with invalid data" do
+      assert {:error, %Ecto.Changeset{}} = Courses.create_course(%{})
+    end
+  end
+
+  describe ".update_course/2" do
+    setup [:create_user, :create_course]
+
+    test "updates the course", %{course: course} do
+      assert {:ok, %Course{} = course} = Courses.update_course(course, %{name: "Cooking"})
+      assert course.name == "Cooking"
     end
 
-    test "update_course/2 with valid data updates the course" do
-      course = course_fixture()
-      assert {:ok, %Course{} = course} = Courses.update_course(course, @update_attrs)
-      assert course.name == "some updated name"
-    end
-
-    test "update_course/2 with invalid data returns error changeset" do
-      course = course_fixture()
-      assert {:error, %Ecto.Changeset{}} = Courses.update_course(course, @invalid_attrs)
+    test "returns error changeset with invalid data", %{course: course} do
+      assert {:error, %Ecto.Changeset{}} = Courses.update_course(course, %{name: 42})
       assert course == Courses.get_course!(course.id)
     end
+  end
 
-    test "delete_course/1 deletes the course" do
-      course = course_fixture()
+  describe ".delete_course/1" do
+    setup [:create_user, :create_course]
+
+    test "deletes the course", %{course: course} do
       assert {:ok, %Course{}} = Courses.delete_course(course)
       assert_raise Ecto.NoResultsError, fn -> Courses.get_course!(course.id) end
     end
+  end
 
-    test "change_course/1 returns a course changeset" do
-      course = course_fixture()
+  describe ".change_course/1" do
+    setup [:create_user, :create_course]
+
+    test "returns a course changeset", %{course: course} do
       assert %Ecto.Changeset{} = Courses.change_course(course)
     end
   end
