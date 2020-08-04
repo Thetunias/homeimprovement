a1044502b3153b7de140b87f0b41c4199b47bb7c
Cain Watson <cainwatson00@gmail.com>
2020-08-04 15:06:46 -0500

    chore: add seeds for course and lesson

--- api/priv/repo/seeds.exs
+++ api/priv/repo/seeds.exs
@@ -10,7 +10,16 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-import HomeImprovement.AccountsFactory
+import HomeImprovement.{AccountsFactory, CoursesFactory}
 
-create_user()
-create_user()
+{:ok, [user: user]} = create_user()
+{:ok, [user: user2]} = create_user()
+
+{:ok, [course: course]} = create_course(user: user)
+{:ok, [course: course2]} = create_course(user: user2)
+
+{:ok, [lesson: _course_lesson]} = create_lesson(user: user, course: course)
+{:ok, [lesson: _course_lesson2]} = create_lesson(user: user, course: course)
+
+{:ok, [lesson: _course2_lesson]} = create_lesson(user: user2, course: course2)
+{:ok, [lesson: _course2_lesson2]} = create_lesson(user: user2, course: course2)
